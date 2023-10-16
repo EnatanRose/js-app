315134a58f70095f4506703a882281b1989a6124
EnatanRose <vanoffgent@gmail.com>
2023-10-16 17:06:00 +0300

    from validation

--- src/components/create.component.js
+++ src/components/create.component.js
@@ -1,5 +1,6 @@
 import {Component} from '../core/component'
 import {Form} from '../core/form'
+import { Validators } from '../core/validators'
 
 export class CreateComponent extends Component{
     constructor(id){
@@ -8,16 +9,22 @@ export class CreateComponent extends Component{
     init(){
         this.$el.addEventListener('submit', this.submitHandler.bind(this))
         this.form = new Form(this.$el,{
-            title: [],
-            fulltext: []
+            title: [Validators.required],
+            fulltext: [Validators.required, Validators.minLength(10)]
         })
     }
     submitHandler(event){
         event.preventDefault()
+        if(this.form.isValid()){
+
+        
         const formData = {
             type: this.$el.type.value,
             ...this.form.value()
         }
         console.log(formData, this.form.value())
+    } else {
+        console.warn('form is invalid')
     }
+}
 }
